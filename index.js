f1390faa42ed1735de67e5807964af180082f4ac
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-05-10 10:29:51 +0600

    single router setup

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 
 
-app.use(cors());
-app.use(express.json());
-
-const name = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${name}:${password}@cluster0.ythezyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ythezyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,9 +23,45 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // Send a ping to confirm a successful connection
+      try {
+        
+            const foodCollection = client.db("DB_food").collection("food");
+
+            app.post('/food', async (req, res) => {
+                  const newProduct = req.body;
+                  const result = await foodCollection.insertOne(newProduct);
+                  res.send(result)
+            })
+
+            app.get('/food', async (req, res) => {
+                  const courser = foodCollection.find();
+                  const result = await courser.toArray();
+                  res.send(result)
+            })
+
+
+            app.get('/food/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) };
+                  const result = await foodCollection.findOne(query);
+                  res.send(result);
+            })
+            app.get('/purchase/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) };
+                  const result = await foodCollection.findOne(query);
+                  res.send(result);
+            })
+
+
+      app.get('/food', (req, res) => {
+          
+      })
+        
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -38,10 +71,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', async (req, res) => {
-      res.send("Spicy King server in running state");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-      console.log(`listening on ${port}`);
+  console.log(`Example app listening on port ${port}`)
 })
